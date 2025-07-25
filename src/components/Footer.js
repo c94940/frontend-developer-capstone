f25a3e4baac34856c30c9d4e34497838e52fd145
Chris Williams <chriswilliams@meta.com>
2025-07-24 19:51:39 -0700

    Added page structure and completed preliminary designs for header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,38 +3,36 @@ import logo from '/Users/chriswilliams/Desktop/dev/coursera/frontend-development
 
 function Footer() {
     return (
-        <footer>
-            <div className="footer-logo">
+        <footer className="footer">
+            <div className="logo-container">
                 <img src={logo} alt="Little Lemon logo" />
             </div>
-            <div className="footer-links">
-                <div className="footer-section">
-                    <h3>Navigation</h3>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="#">Reservations</a></li>
-                        <li><a href="#">Order Online</a></li>
-                        <li><a href="#">Login</a></li>
-                    </ul>
-                </div>
-                <div className="footer-section">
-                    <h3>Contact</h3>
-                    <ul>
-                        <li>123 Easy St, San Diego, CA, 92107</li>
-                        <li>+1 (619) 555-5555</li>
-                        <li>info@littlelemon.com</li>
-                    </ul>
-                </div>
-                <div className="footer-section">
-                    <h3>Social Media</h3>
-                    <ul>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Instagram</a></li>
-                        <li><a href="#">TikTok</a></li>
-                    </ul>
-                </div>
+            <div className="footer-container">
+                <h5>Navigation</h5>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservations</a></li>
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+            <div className="footer-container">
+                <h5>Contact</h5>
+                <ul>
+                    <li>123 Easy St, San Diego, CA, 92107</li>
+                    <li>+1 (619) 555-5555</li>
+                    <li>info@littlelemon.com</li>
+                </ul>
+            </div>
+            <div className="footer-container">
+                <h5>Social Media</h5>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">TikTok</a></li>
+                </ul>
             </div>
         </footer>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,31 @@
+import './Main.css';
+import heroimage from '../../src/assets/restaurantfood.jpg';
+
 function Main() {
     return (
         <main>
-
+            <div className="hero">
+                <div className="flex-container">
+                    <h1>Little Lemon</h1>
+                    <h2>San Diego</h2>
+                    <p>Welcome to Little Lemon, your gateway to the vibrant flavors of the Mediterranean! Experience a delightful culinary journey featuring fresh, wholesome ingredients inspired by the sun-kissed coasts of Greece, Italy, Spain, and beyond.</p>
+                    <button>Make a reservation</button>
+                </div>
+                <div className="flex-container">
+                    <img src={heroimage} alt="Server holding fresh food" />
+                </div>
+            </div>
+            <div className="specials">
+                <h3>This week's specials</h3>
+                <button>Online menu</button>
+                <div className="special-cards">
+                    <p>Special Card</p>
+                    <p>Special Card</p>
+                    <p>Special Card</p>
+                </div>
+            </div>
+            <div className="customer-reviews">Customer Reviews</div>
+            <div className="about">About</div>
         </main>
     );
 }
