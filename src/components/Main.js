7dba101d7911e0be1f38ad99b4d66089ab50a099
Chris Williams <chriswilliams@meta.com>
2025-07-24 23:06:15 -0700

    Worked on Specials and Customer Review sections

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,26 +5,50 @@ function Main() {
     return (
         <main>
             <div className="hero">
-                <div className="flex-container">
+                <div className="hero-flex-container">
                     <h1>Little Lemon</h1>
                     <h2>San Diego</h2>
                     <p>Welcome to Little Lemon, your gateway to the vibrant flavors of the Mediterranean! Experience a delightful culinary journey featuring fresh, wholesome ingredients inspired by the sun-kissed coasts of Greece, Italy, Spain, and beyond.</p>
                     <button>Make a reservation</button>
                 </div>
-                <div className="flex-container">
+                <div className="hero-image-container">
                     <img src={heroimage} alt="Server holding fresh food" />
                 </div>
             </div>
             <div className="specials">
-                <h3>This week's specials</h3>
-                <button>Online menu</button>
-                <div className="special-cards">
-                    <p>Special Card</p>
-                    <p>Special Card</p>
-                    <p>Special Card</p>
+                <div className="specials-menu-container">
+                    <h3>This week's specials</h3>
+                    <button>View menu</button>
+                </div>
+                <div className="special-card-container">
+                    <div className="special-card">
+                        <p>Special Card</p>
+                    </div>
+                    <div className="special-card">
+                        <p>Special Card</p>
+                    </div>
+                    <div className="special-card">
+                        <p>Special Card</p>
+                    </div>
                 </div>
             </div>
-            <div className="customer-reviews">Customer Reviews</div>
+            <div className="customer-reviews">
+                <h3>Customer Reviews</h3>
+                <div className="review-card-container">
+                    <div className="customer-review-card">
+                        <p>Customer Review Card</p>
+                    </div>
+                    <div className="customer-review-card">
+                        <p>Customer Review Card</p>
+                    </div>
+                    <div className="customer-review-card">
+                        <p>Customer Review Card</p>
+                    </div>
+                    <div className="customer-review-card">
+                        <p>Customer Review Card</p>
+                    </div>
+                </div>
+            </div>
             <div className="about">About</div>
         </main>
     );
