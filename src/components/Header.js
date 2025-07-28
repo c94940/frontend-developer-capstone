3552aae2342af4b19d59953aaaf1d94de41cd85b
Chris Williams <c94940@gmail.com>
2025-07-27 22:36:55 -0700

    Made Header responsive with media query for screen width ≤ 768px

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Nav from './Nav';
 import logo from '../assets/Logo.svg';
 import './Header.css';
 
-function Header() {
+const Header = () => {
     return (
         <header className="header">
             <div className="logo-container">
